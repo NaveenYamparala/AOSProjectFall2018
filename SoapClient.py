b31754e4066105cf8570c748c0b606f30116f74b
Naveen Yamparala <naveenyamparala@gmail.com>
2018-11-30 17:02:55 -0500

    Updated SoapClient and Server files

---
+++ SoapClient.py
@@ -0,0 +1,7 @@
+from suds.client import Client
+#url="http://www.thomas-bayer.com/axis2/services/BLZService?wsdl"
+url = 'http://127.0.0.1:5000/aosprojectservices?wsdl'
+client = Client(url)
+#print client ## shows the details of this service
+
+print client.service.stringReverse("Naveen")
